2e97215db2b9cb3ad1511a030f861a6293b3d11f
neslanovic <timy1901@gmail.com>
2019-07-26 19:42:16 +0200

    two section ready

--- js/script.js
+++ js/script.js
@@ -7,16 +7,46 @@ window.onscroll = () => {
     nav.classList.remove('scrolled-nav');
   }
 };
+
 // header button mouse event--over and leave
 $('.header-btn').mouseover(() => {
   setTimeout(() => {
     $('.header-btn')
-      .css('background-color', ' #ffd344');
+      .css({ 'background-color': ' #ffd344', 'border-color': ' #ffd344' });
   }, 300);
 });
 $('.header-btn').mouseleave(() => {
   setTimeout(() => {
     $('.header-btn')
-      .css('background-color', 'transparent');
+      .css({ 'background-color': 'transparent', 'border-color': '#ffffff' });
   }, 300);
 });
+
+// TestImonial
+const testImonial = {
+  data: [],
+  init() {
+    this.findData();
+  },
+  findData() {
+    $.getJSON('http://46.101.237.11/json/users.json', (result) => {
+      this.data = result;
+      console.log(this.data);
+    });
+    this.showAll();
+  },
+  showAll() {
+    let div = '';
+    for (let i = 0; i < this.data.length; i += 1) {
+      div += this.divMaker(this.data[i], i);
+    }
+    document.querySelector('.test').innerHTML = div;
+  },
+  divMaker(person, index) {
+    return `<div>
+    ${person.name.first}
+</div>`;
+  },
+
+};
+testImonial.init();
